a6ca3c5895300fb5ebd04a8fb20636a71f7f28a7
skyeat738 <skynetitsolutions49@gmail.com>
2026-09-07 17:39:44 +0530

    Update link text from 'Explore Our Story' to 'Explore Our Stories'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@
                     <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path><line x1="3" y1="6" x2="21" y2="6"></line><path d="M16 10a4 4 0 0 1-8 0"></path></svg>
                     Shop Namkeen
                 </a>
-                <a href="#our-story" class="btn btn--outline">Explore Our Story</a>
+                <a href="#our-story" class="btn btn--outline">Explore Our Stories</a>
             </div>
         </div>
 
